fe7081bc475e956ab574f16fb827049662589c92
Awais S. Khan <awais.khan725@gmail.com>
2020-06-20 20:48:17 +1000

    Updates

---
+++ precache-manifest.d3855f538472c12cca0acd4ce70ff7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a8af46da56bf38b2c2317801f59848",
+    "url": "/faceDetect/index.html"
+  },
+  {
+    "revision": "bcdd3a91b8dd38d1304f",
+    "url": "/faceDetect/static/css/main.75b96901.chunk.css"
+  },
+  {
+    "revision": "240e859cb7cc8e818cfe",
+    "url": "/faceDetect/static/js/2.d7fac8ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceDetect/static/js/2.d7fac8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdd3a91b8dd38d1304f",
+    "url": "/faceDetect/static/js/main.7a420b44.chunk.js"
+  },
+  {
+    "revision": "198d538a25bc808f63a5",
+    "url": "/faceDetect/static/js/runtime-main.30a7d457.js"
+  }
+]);
